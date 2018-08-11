cf35ae3aec20ae62d6a2263763493c5cd6b61949
Rodrigo Teles <rodrigo@Rodrigos-MacBook-Air.local>
2018-08-11 11:00:32 +0200

    changed traffic

--- openapps/uinject/uinject.c
+++ openapps/uinject/uinject.c
@@ -24,7 +24,7 @@ uint8_t prefix[8] = {0xbb, 0xbb, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 uint32_t seqnum = 0;
 
-uint32_t traffic_rates[5] = {2000,4000,8000,16000,32000,64000,128000};
+uint32_t traffic_rates[5] = {3000,6500,10000,13500,17000,20500};
 uint32_t current_traffic_rate;
 
 //=========================== prototypes ======================================
